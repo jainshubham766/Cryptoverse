2c69c0cd8a5112f95218a2cd8c0b917aa647ba07
Shubham Jain <jainshubham766@gmail.com>
2022-03-12 23:55:55 +0530

    Added Crypto News Module- Key Replacement Required.

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,10 +1,11 @@
 import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 const cryptoApiHeaders = {
     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-    'x-rapidapi-key': '[ADD PRIVATE KEY HERE]'
+    'x-rapidapi-key': '785f6fbd62msh849a48de42e73fap168883jsn876be5357019'
 }
 
-
+//66010bf85cmshc3faf34c8f80160p1230afjsn6800947f9eeb
+//785f6fbd62msh849a48de42e73fap168883jsn876be5357019
 const baseUrl = 'https://coinranking1.p.rapidapi.com';
 
 const createRequest = (url) => ({url, headers: cryptoApiHeaders})
