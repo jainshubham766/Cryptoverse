a0fe2d999a576f44552a2805938e0b6d81923717
Shubham Jain <jainshubham766@gmail.com>
2022-03-12 14:43:40 +0530

    Key Error Resolved, Added New Module

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,7 @@
-import {configureStore} from '@reduxjs/toolkit';
-import { cryptoApi } from '../services/cryptoApi';
-
-
-
-export default configureStore(
-    {
-        reducer:{
-            [cryptoApi.reducerPath]: cryptoApi.reducer,
-        },
-    }
-)
+import { configureStore } from "@reduxjs/toolkit";
+import { cryptoApi } from "../services/cryptoApi";
+export default configureStore({
+    reducer: {
+        [cryptoApi.reducerPath]: cryptoApi.reducer,
+    },
+});
